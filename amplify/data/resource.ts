e95d17bbd2acfb9826fc3ca892dfc4ec5401a230
Diego Barrales <diego.barrales@csu.fullerton.edu>
2024-11-20 20:52:57 -0800

    Fixed Highscore issue

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,7 +3,7 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 const schema = a.schema({
   Players: a.model({
       username: a.string().required(),
-      highscore: a.float().required().default(0.00),
+      highscore: a.float().required(),
       achievedAt: a.date().default(new Date().toISOString())
     })
     .authorization((allow) => [allow.publicApiKey()]),
